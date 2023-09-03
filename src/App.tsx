ffdf489bba9594445fa28b9ff58e29c871634f20
madsab <mads.barnes@hotmail.com>
2023-09-03 22:05:44 +0200

    Centered content

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Banner from "./components/sections/Banner";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-theme-gray to-theme-brown text-white">
+    <div className="bg-gradient-to-r from-theme-gray to-theme-brown text-white flex justify-center">
       <Banner />
     </div>
   );
