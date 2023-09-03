110e3b15ee39ab73c345f538d623033ba52e1332
madsab <mads.barnes@hotmail.com>
2023-09-03 21:07:00 +0200

    Popup + slide animation

--- src/components/animation/Reveal.tsx
+++ src/components/animation/Reveal.tsx
@@ -1,24 +1,56 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { once } from "events";
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 
 interface Props {
   children: JSX.Element;
-  width?: "object-cover" | "100%";
+  width?: "fit-content" | "100%";
 }
 
-export const Reveal = ({ children, width = "object-cover" }: Props) => {
+export const Reveal = ({ children, width = "fit-content" }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainController = useAnimation();
+  const slideController = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainController.start("visible");
+      slideController.start("visible");
+    }
+  }, [isInView]);
   return (
-    <div className={width + " relative hidden"}>
+    <div ref={ref} style={{ position: "relative", overflow: "hidden", width }}>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75 },
           visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
-        animate="visible"
+        animate={mainController}
+        transition={{ duration: 0.5, delay: 0.25 }}
       >
         {children}
       </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideController}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          background: "#222138",
+          zIndex: "20",
+        }}
+      />
     </div>
   );
 };
