9653e720e678b9541d056404d1c13ab2079cb77d
madsab <mads.barnes@hotmail.com>
2023-09-03 22:06:07 +0200

    Info and headline for banner

--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -3,18 +3,30 @@ import { Reveal } from "../animation/Reveal";
 
 const Banner = () => {
   return (
-    <section className="min-h-screen flex items-center ">
-      <div className="felx flex-col">
+    <section className="min-h-screen flex items-center ml-7  ">
+      <div className="felx flex-col space-y-2">
         <Reveal>
-          <p className="text-9xl animate-bounce font-bold">Hello There!</p>
+          <p className="text-9xl font-bold">Hello There!</p>
         </Reveal>
-        <p className="text-3xl">I&apos;m Mads, a fronted developer</p>
-        <p className="max-w-2xl">
-          Currently studying a 3 year bachelor degree in Informatics at
-          "Norwegian University of Science and Technology" in Trondheim. This is
-          a site showing of my skills and projects in software development. If
-          you&apos;d like to know more, please contact me.
-        </p>
+        <Reveal>
+          <p className="text-3xl">
+            I&apos;m Mads, a{" "}
+            <span className="text-theme-lightgray">Fronted Developer</span>
+          </p>
+        </Reveal>
+        <Reveal>
+          <p className="max-w-2xl">
+            Currently studying a 3 year bachelor degree in Informatics at
+            "Norwegian University of Science and Technology" in Trondheim. This
+            is a site showing of my skills and projects in software development.
+            If you&apos;d like to know more, please contact me.
+          </p>
+        </Reveal>
+        <Reveal>
+          <button className="border-solid border-black border  hover:bg-theme-green">
+            Contact Me
+          </button>
+        </Reveal>
       </div>
     </section>
   );
