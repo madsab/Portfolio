ce8deb398dc0317cdd6bf0ba280d391da2dca1a0
madsab <mads.barnes@hotmail.com>
2023-09-04 14:22:43 +0200

    New Navbar

---
+++ src/components/sections/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as BRouter } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <BRouter>
+      <div className="flex ">
+        <p>Mads B&#229;rnes</p>
+        <p></p>
+      </div>
+    </BRouter>
+  );
+};
+
+export default Navbar;
