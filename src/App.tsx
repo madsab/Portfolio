67b8375c0fcd3b45bf01ad71306e30b9d9e48247
madsab <mads.barnes@hotmail.com>
2023-09-04 14:21:31 +0200

    added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Banner from "./components/sections/Banner";
+import Navbar from "./components/sections/Navbar";
 
 function App() {
   return (
     <div className="bg-gradient-to-r from-theme-gray to-theme-brown text-white flex justify-center">
+      <Navbar />
       <Banner />
     </div>
   );
