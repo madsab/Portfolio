bde51db33577a29e207fb5f6cf1541b0eee72c89
madsab <mads.barnes@hotmail.com>
2023-09-07 20:39:53 +0200

    New button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+const Button = ({ ...props }) => {
+  return (
+    <button className=" relative border-2 p-2 rounded-md bg-transparent transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-theme-blue before:transition-transform before:duration-300 before:content-[''] before:hover:scale-x-100">
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
