18d9e62d2f70172604d63e7676c5255910991e50
madsab <mads.barnes@hotmail.com>
2023-09-11 09:35:40 +0200

    added sifu img and projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import Banner from "./components/sections/Banner";
-import Navbar from "./components/sections/Navbar";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import SifuBg from "./assets/img/sifuStaff.png";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-theme-gray to-theme-brown text-white flex justify-center">
+    <div className="bg-gradient-to-r from-theme-gray to-theme-brown text-white">
       <Navbar />
+      <img
+        className="absolute"
+        src={SifuBg}
+        style={{
+          aspectRatio: "2/2",
+          height: "500vh",
+          left: "20vw",
+          overflow: "scroll",
+        }}
+        alt="Sifu character"
+      />
       <Banner />
+      <Projects />
     </div>
   );
 }
