311ba3ac3cde2f4631bc7023e5077159b3366530
madsab <mads.barnes@hotmail.com>
2023-09-11 09:36:13 +0200

    Layout complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,27 +2,29 @@ import React from "react";
 import { BrowserRouter as BRouter } from "react-router-dom";
 import Button from "./atoms/Button";
 import Reveal from "./animation/Reveal";
-// import { ReactComponent as GithubLogo } from "../assets/img/githubLogo.png";
+import githublogo from "../assets/img/githubLogo.png";
+import linkdnLogo from "../assets/img/linkdnLogo.png";
 
 const Navbar = () => {
   return (
-    <Reveal downwards={true} slider={false} width="100%" delay={0.5}>
-      <BRouter>
-        <div className="flex h-20 items-center pr-6 pl-6">
-          <p className="grow flex justify-center text-xl ">About</p>
-          <p className="grow flex justify-center text-xl">Skills</p>
-          <p className="grow flex justify-center text-5xl pt-3 ">
-            Mads B&#229;rnes
-          </p>
-          <p className="grow flex justify-center text-xl">Projects</p>
-          <div className="grow flex justify-center">
+    <div className="sticky top-0 pr-6 pl-6 z-30">
+      <Reveal downwards={true} slider={false} width="100%" delay={0.5}>
+        <nav className="flex h-20 items-center">
+          <div className="grow flex space-x-10">
+            <p className="flex justify-center text-xl ">About</p>
+            <p className="flex justify-center text-xl">Skills</p>
+            <p className="flex justify-center text-xl">Projects</p>
+          </div>
+          <div className="grow flex justify-end">
+            <img src={linkdnLogo} alt="Linkdn logo" width={45} />
+            <img src={githublogo} alt="Github logo" width={45} />
             <Button>
               <span className="text-2xl">Contact Me</span>
             </Button>
           </div>
-        </div>
-      </BRouter>
-    </Reveal>
+        </nav>
+      </Reveal>
+    </div>
   );
 };
 
