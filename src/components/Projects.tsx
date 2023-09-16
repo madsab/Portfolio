66459e5fadd5792186085bc1f1f023590795d71b
madsab <mads.barnes@hotmail.com>
2023-09-16 21:24:54 +0200

    projects layout finished + dummy values

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import stoneImg from "../assets/img/StoneWebsite.png";
+import ProjectCard from "./atoms/ProjectCard";
 
 const Projects = () => {
-  return <div className="w-full min-h-screen"></div>;
+  const projects = [
+    {
+      title: "STONE - SpyWebsite",
+      description:
+        "School Project: Frontend design and development - Draw Whey icon to Dag's hand",
+      imgUrl: stoneImg,
+      siteUrl: "https://folk.ntnu.no/madsab/STONE/website/index.html",
+    },
+    {
+      title: "STONE - SpyWebsite",
+      description:
+        "School Project: Frontend design and development - Draw Whey icon to Dag's hand",
+      imgUrl: stoneImg,
+      siteUrl: "https://folk.ntnu.no/madsab/STONE/website/index.html",
+    },
+    {
+      title: "STONE - SpyWebsite",
+      description:
+        "School Project: Frontend design and development - Draw Whey icon to Dag's hand",
+      imgUrl: stoneImg,
+      siteUrl: "https://folk.ntnu.no/madsab/STONE/website/index.html",
+    },
+  ];
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <p className="text-5xl mb-3 font-bold">My Projects</p>
+      <div className="border-2 rounded-xl w-[90%] flex flex-wrap shadow-2xl">
+        {projects.map((p, index) => {
+          return <ProjectCard key={index} {...p} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
