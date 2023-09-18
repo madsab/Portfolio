aa76130519f820ba998f35d6fe8e2056468f54bc
madsab <mads.barnes@hotmail.com>
2023-09-18 15:17:03 +0200

    Reveal animation on projectCard

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import stoneImg from "../assets/img/StoneWebsite.png";
 import ProjectCard from "./atoms/ProjectCard";
+import Reveal from "./animation/Reveal";
 
 const Projects = () => {
   const projects = [
@@ -29,8 +30,10 @@ const Projects = () => {
 
   return (
     <div className="w-full flex flex-col items-center" id="projects">
-      <p className="text-5xl mb-3 font-bold">My Projects</p>
-      <div className="border-2 rounded-xl w-[90%] flex flex-wrap shadow-2xl">
+      <Reveal>
+        <p className="text-5xl mb-3 font-bold">My Projects</p>
+      </Reveal>
+      <div className="border-2 rounded-xl w-[90%] flex justify-center flex-wrap shadow-2xl">
         {projects.map((p, index) => {
           return <ProjectCard key={index} {...p} />;
         })}
