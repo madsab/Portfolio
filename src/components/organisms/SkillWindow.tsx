be37d2b3e66d28e2a2164e84d3a0e618b5d62929
madsab <mads.barnes@hotmail.com>
2023-09-19 14:59:04 +0200

    added Popup animation

--- src/components/organisms/SkillWindow.tsx
+++ src/components/organisms/SkillWindow.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 import SkillCard from "../atoms/SkillCard";
+import PopUp from "../animation/PopUp";
 
 interface Props {
   aquiredSkills: any[];
   trigger: boolean;
 }
 const SkillWindow = ({ aquiredSkills, trigger = false }: Props) => {
-  return trigger ? (
-    <div className="absolute w-[50%] top-[15%] left-[27.5%] bg-slate-950 shadow-md shadow-slate-400 rounded-lg h-[30%] flex items-center justify-center space-x-6">
-      {aquiredSkills.map((skill, index) => {
-        return (
-          <SkillCard
-            key={index}
-            title={skill.title}
-            icon={skill.icon}
-            alt={skill.alt}
-          />
-        );
-      })}
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <PopUp fire={trigger}>
+        <div className="absolute w-[50%] top-[15%] left-[27.5%] bg-slate-950 shadow-md shadow-slate-400 rounded-lg h-[30%] flex items-center justify-center space-x-6">
+          {aquiredSkills.map((skill, index) => {
+            return (
+              <SkillCard
+                key={index}
+                title={skill.title}
+                icon={skill.icon}
+                alt={skill.alt}
+              />
+            );
+          })}
+        </div>
+      </PopUp>
     </div>
-  ) : (
-    ""
   );
 };
 
