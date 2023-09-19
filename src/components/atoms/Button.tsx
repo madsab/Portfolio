3ecf6ca59f75564d1d74a4dddbcec0b2ad8f1328
madsab <mads.barnes@hotmail.com>
2023-09-19 14:57:51 +0200

    button styling

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ ...props }) => {
   return (
-    <button className=" relative border-2 p-2 rounded-3xl transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-theme-blue before:transition-transform before:duration-300 before:content-[''] before:hover:scale-x-100 before:rounded-3xl">
+    <button className=" relative overflow-hidden border-2 p-2 rounded-3xl transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-theme-blue before:transition-transform before:duration-300 before:content-[''] before:hover:scale-x-100 before:rounded-3xl">
       {props.children}
     </button>
   );
