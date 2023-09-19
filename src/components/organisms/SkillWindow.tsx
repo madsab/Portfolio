37b37c443af364d3269089b6c4293af719e372c1
madsab <mads.barnes@hotmail.com>
2023-09-19 19:49:05 +0200

    justify even

--- src/components/organisms/SkillWindow.tsx
+++ src/components/organisms/SkillWindow.tsx
@@ -12,7 +12,7 @@ const SkillWindow = ({ aquiredSkills, trigger = false }: Props) => {
       <PopUp fire={trigger}>
         <div className="relative bg-slate-950 p-6 shadow-md shadow-slate-400 rounded-lg h-[30%] flex flex-col items-center justify-center">
           <p className="text-2xl mb-5">My Programming Languages</p>
-          <div className="flex w-full">
+          <div className="flex w-full justify-evenly ">
             {aquiredSkills.map((skill, index) => {
               return (
                 <SkillCard
