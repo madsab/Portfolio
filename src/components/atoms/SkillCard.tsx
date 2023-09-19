ea19742da5ff716dd95b949267e4ec75dacfe2a3
madsab <mads.barnes@hotmail.com>
2023-09-19 19:46:05 +0200

    3D box layout

--- src/components/atoms/SkillCard.tsx
+++ src/components/atoms/SkillCard.tsx
@@ -3,9 +3,14 @@ import React from "react";
 const SkillCard = ({ title, icon, alt }: any) => {
   const iconSrc = icon ? icon : "";
   return (
-    <div>
-      <img src={iconSrc} alt={alt} width={45} />
-      <p>{title}</p>
+    <div className="relative">
+      <div className=" relative bg-slate-950 flex flex-col items-center justify-center m-3 border-2 rounded-lg w-24 h-24 z-10 ">
+        <div className=" rounded-lg overflow-hidden drop-shadow-3d-white ">
+          <img src={iconSrc} alt={alt} width={45} />
+        </div>
+        <p>{title}</p>
+      </div>
+      <div className="absolute top-3.5 left-5 w-24 h-24 bg-white rounded-lg"></div>
     </div>
   );
 };
