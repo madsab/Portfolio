1bb135183b2eaca436d5a1cada2b1b6c06f336e4
madsab <mads.barnes@hotmail.com>
2023-09-19 19:47:37 +0200

    Skill header and box adjustment

--- src/components/organisms/SkillWindow.tsx
+++ src/components/organisms/SkillWindow.tsx
@@ -8,19 +8,22 @@ interface Props {
 }
 const SkillWindow = ({ aquiredSkills, trigger = false }: Props) => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="absolute w-[50%] flex flex-col left-[27%] top-[20%]">
       <PopUp fire={trigger}>
-        <div className="absolute w-[50%] top-[15%] left-[27.5%] bg-slate-950 shadow-md shadow-slate-400 rounded-lg h-[30%] flex items-center justify-center space-x-6">
-          {aquiredSkills.map((skill, index) => {
-            return (
-              <SkillCard
-                key={index}
-                title={skill.title}
-                icon={skill.icon}
-                alt={skill.alt}
-              />
-            );
-          })}
+        <div className="relative bg-slate-950 p-6 shadow-md shadow-slate-400 rounded-lg h-[30%] flex flex-col items-center justify-center">
+          <p className="text-2xl mb-5">My Programming Languages</p>
+          <div className="flex w-full">
+            {aquiredSkills.map((skill, index) => {
+              return (
+                <SkillCard
+                  key={index}
+                  title={skill.title}
+                  icon={skill.icon}
+                  alt={skill.alt}
+                />
+              );
+            })}
+          </div>
         </div>
       </PopUp>
     </div>
