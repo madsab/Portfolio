c21ed272e650aca71006531489e5e1996f1ee6b3
madsab <mads.barnes@hotmail.com>
2023-09-25 11:29:41 +0200

    Changed to SkillCarousel

--- src/components/organisms/SkillWindow.tsx
+++ src/components/organisms/SkillWindow.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SkillCard from "../atoms/SkillCard";
 import PopUp from "../animation/PopUp";
+import SkillCarousel from "./SkillCarousel";
 
 interface Props {
   aquiredSkills: any[];
@@ -8,21 +9,12 @@ interface Props {
 }
 const SkillWindow = ({ aquiredSkills, trigger = false }: Props) => {
   return (
-    <div className="absolute w-[50%] flex flex-col left-[27%] top-[20%]">
+    <div className="absolute w-[50%]  left-[27%] top-[20%]">
       <PopUp fire={trigger}>
         <div className="relative bg-slate-950 p-6 shadow-md shadow-slate-400 rounded-lg h-[30%] flex flex-col items-center justify-center">
           <p className="text-2xl mb-5">My Programming Languages</p>
-          <div className="flex w-full justify-evenly ">
-            {aquiredSkills.map((skill, index) => {
-              return (
-                <SkillCard
-                  key={index}
-                  title={skill.title}
-                  icon={skill.icon}
-                  alt={skill.alt}
-                />
-              );
-            })}
+          <div className="w-full ">
+            <SkillCarousel items={aquiredSkills} />
           </div>
         </div>
       </PopUp>
