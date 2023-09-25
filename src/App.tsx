5fbbc4605d45d82ad9abc4caba0a0ff48e9b806f
madsab <mads.barnes@hotmail.com>
2023-09-25 12:55:55 +0200

    added Construction pop-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import SifuBg from "./assets/img/sifuStaff.png";
 import Skills from "./components/Skills";
+import PopUp from "./components/animation/PopUp";
 
 function App() {
+  const [trigger, setTrigger] = useState(true);
   return (
     <div
       id="top"
@@ -23,6 +25,24 @@ function App() {
         }}
         alt="Sifu character"
       /> */}
+      <div className=" sticky top-[30%] left-[15%] z-30 ">
+        <PopUp fire={trigger}>
+          <div className="bg-black rounded-lg flex flex-col items-center justify-center p-10 space-y-5">
+            <p className="text-6xl">UNDER CONSTRUCTION!</p>
+            <p>
+              This site is currently under construction. Because of this some
+              features may not be complete or not been added. More updates
+              coming soon
+            </p>
+            <button
+              className="border-2 p-3 rounded-lg"
+              onClick={() => setTrigger(false)}
+            >
+              Close
+            </button>
+          </div>
+        </PopUp>
+      </div>
 
       <Banner />
       <Skills />
