241c2021619e55c9a59506c5c59f62c5b20d304c
madsab <mads.barnes@hotmail.com>
2023-09-25 11:29:05 +0200

    talwindcss

--- src/components/atoms/SkillCard.tsx
+++ src/components/atoms/SkillCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SkillCard = ({ title, icon, alt }: any) => {
   const iconSrc = icon ? icon : "";
   return (
-    <div className="relative">
+    <div className="relative w-32 inline-flex items-center justify-center m-3">
       <div className=" relative bg-slate-950 flex flex-col items-center justify-center m-3 border-2 rounded-lg w-24 h-24 z-10 ">
         <div className=" rounded-lg overflow-hidden drop-shadow-3d-white ">
           <img src={iconSrc} alt={alt} width={45} />
