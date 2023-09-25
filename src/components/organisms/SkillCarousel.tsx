fcd7afbda8344032dedf18718e9ba45333635e21
madsab <mads.barnes@hotmail.com>
2023-09-25 12:12:20 +0200

    removed import, no pointer events on SkillCards

--- src/components/organisms/SkillCarousel.tsx
+++ src/components/organisms/SkillCarousel.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SkillCard from "../atoms/SkillCard";
 
 interface Props {
   items: any[];
 }
 const SkillCarousel = ({ items }: Props) => {
-  useEffect(() => {});
   return (
     <div className="overflow-hidden w-full flex border-2  border-slate-400 shadow-inner shadow-slate-600 whitespace-nowrap cursor-grab ">
-      <div className=" animate-slider ">
+      <div className=" animate-slider pointer-events-none ">
         {items.map((child, index) => {
           return (
             <SkillCard
@@ -20,7 +19,7 @@ const SkillCarousel = ({ items }: Props) => {
           );
         })}
       </div>
-      <div className=" animate-slider ">
+      <div className=" animate-slider pointer-events-none ">
         {items.map((child, index) => {
           return (
             <SkillCard
