122a3c020db7b78f84cef718eeaf4600acb4a5ff
madsab <mads.barnes@hotmail.com>
2023-09-27 16:30:19 +0200

    enabeld styling prop

--- src/components/organisms/SkillWindow.tsx
+++ src/components/organisms/SkillWindow.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import SkillCard from "../atoms/SkillCard";
 import PopUp from "../animation/PopUp";
 import SkillCarousel from "./SkillCarousel";
 
 interface Props {
   aquiredSkills: any[];
   trigger: boolean;
+  className?: string;
+  headLine: string;
 }
-const SkillWindow = ({ aquiredSkills, trigger = false }: Props) => {
+const SkillWindow = ({
+  aquiredSkills,
+  trigger = false,
+  className,
+  headLine,
+}: Props) => {
   return (
-    <div className="absolute w-[50%]  left-[27%] top-[20%]">
+    <div className={`${className}`}>
       <PopUp fire={trigger}>
         <div className="relative bg-slate-950 p-6 shadow-md shadow-slate-400 rounded-lg h-[30%] flex flex-col items-center justify-center">
-          <p className="text-2xl mb-5">My Programming Languages</p>
+          <p className="text-2xl mb-5">{headLine}</p>
           <div className="w-full ">
             <SkillCarousel items={aquiredSkills} />
           </div>
