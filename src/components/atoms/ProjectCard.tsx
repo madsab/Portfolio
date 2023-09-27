2f571b1b50ed04218631d7813af718959e396f8a
madsab <mads.barnes@hotmail.com>
2023-09-27 19:58:23 +0200

    fixed header lineup issue

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -15,7 +15,7 @@ const ProjectCard = ({ title, description, imgUrl, siteUrl }: any) => {
             alt="Stone School project website"
           />
           <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70 "></div>
-          <div className="absolute inset-0 flex flex-col items-center justify-center px-9 text-center translate-y-[60%] group-hover:translate-y-0 transition-all">
+          <div className="absolute inset-0 flex flex-col items-center px-9 text-center translate-y-[75%] group-hover:translate-y-0 group-hover:justify-center transition-all">
             <p className="text-3xl font-bold mb-3">{title}</p>
             <p className="text-lg font-light italic text-slate-300">
               {description}
