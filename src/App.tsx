544a1ad76e12ca1559e19689827ab9d15092a030
madsab <mads.barnes@hotmail.com>
2023-09-28 11:03:02 +0200

    removed popu, added Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import PopUp from "./components/animation/PopUp";
 import SifuCharacter from "./components/SifuCharacter";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
-  const [trigger, setTrigger] = useState(true);
   return (
     <div
       id="top"
@@ -16,29 +15,11 @@ function App() {
     >
       <SifuCharacter />
       <Navbar />
-      <div className=" sticky top-[30%] left-[15%] z-30 ">
-        <PopUp fire={trigger}>
-          <div className="bg-black rounded-lg flex flex-col items-center justify-center p-10 space-y-5">
-            <p className="text-6xl">UNDER CONSTRUCTION!</p>
-            <p>
-              This site is currently under construction. Because of this some
-              features may not be complete or not been added. More updates
-              coming soon
-            </p>
-            <button
-              className="border-2 p-3 rounded-lg"
-              onClick={() => setTrigger(false)}
-            >
-              Close
-            </button>
-          </div>
-        </PopUp>
-      </div>
-
       <Banner />
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
