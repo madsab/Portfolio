cca452bd1478eeb1838125343f93d7b223a11292
madsab <mads.barnes@hotmail.com>
2023-09-29 11:10:08 +0200

    New styling + harry potter

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,24 +2,27 @@ import React from "react";
 import Input from "./atoms/Input";
 import Button from "./atoms/Button";
 
+import HarryPotter from "../assets/img/HarryPotter.png";
+
 const Contact = () => {
   return (
     <div
       id="contact"
       className="w-full min-h-screen flex justify-center items-center "
     >
-      <div className="flex flex-col h-fit bg-theme-lightgray rounded-lg justify-center items-center border-2 shadow-3xl">
+      <div className="w-[40%]">
+        <img src={HarryPotter} alt="Harry Potter" />
+      </div>
+      <div className="flex flex-col h-fit bg-theme-lightgray rounded-lg justify-center items-center border-2 p-6 shadow-3xl">
         <p className="text-theme-blue  ">Have a question?</p>
         <p className="text-2xl p-2">
           Send me a message and i&apos;ll answer as best i can!
         </p>
-        <div className="flex w-[90%] ">
+        <div className="flex w-[90%] mt-2 ">
           <div>
-            <p>Name:</p>
             <Input type="text" placeholder="Your full name" />
           </div>
           <div>
-            <p>Email:</p>
             <Input type="email" placeholder="Your Email" />
           </div>
         </div>
