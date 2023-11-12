d8bce733bffb78eee05095c8b1b962b7b6fa75c9
madsab <mads.barnes@hotmail.com>
2023-11-12 21:22:16 +0100

    Navbar links

---
+++ src/components/utils/MenuLinks.tsx
@@ -0,0 +1,16 @@
+const MenuLinks = [
+  {
+    label: "About",
+    to: "about",
+  },
+  {
+    label: "Projects",
+    to: "projects",
+  },
+  {
+    label: "Contact",
+    to: "contact",
+  },
+];
+
+export default MenuLinks;
