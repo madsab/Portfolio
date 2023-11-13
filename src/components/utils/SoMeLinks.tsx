4eeb5c24005212f8d18dfc286c9a31e147b147cc
madsab <mads.barnes@hotmail.com>
2023-11-13 09:02:00 +0100

    Social Media links

---
+++ src/components/utils/SoMeLinks.tsx
@@ -0,0 +1,14 @@
+export const SoMeLinks = [
+  {
+    logo: "akar-icons:github-fill",
+    url: "https://github.com/madsab",
+  },
+  {
+    logo: "ri:facebook-fill",
+    url: "https://www.facebook.com/profile.php?id=100007659018139",
+  },
+  {
+    logo: "akar-icons:linkedinv2-fill",
+    url: "https://www.linkedin.com/in/mads-b%C3%A5rnes-a66b96272/",
+  },
+];
