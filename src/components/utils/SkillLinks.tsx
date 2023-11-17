7f5da8edaa5ddd837e2bd34590bedd7f4d23c499
madsab <mads.barnes@hotmail.com>
2023-11-18 00:20:16 +0100

    List of my skills

---
+++ src/components/utils/SkillLinks.tsx
@@ -0,0 +1,74 @@
+export const FrontendSkills = [
+  {
+    name: "React",
+    icon: "simple-icons:react",
+    alt: "React logo",
+    url: "https://reactjs.org/",
+  },
+  {
+    name: "HTML",
+    icon: "simple-icons:html5",
+    alt: "HTML logo",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    name: "CSS",
+    icon: "simple-icons:css3",
+    alt: "CSS logo",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    name: "TailwindCSS",
+    icon: "simple-icons:tailwindcss",
+    alt: "TailwindCSS logo",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    name: "TypeScript",
+    icon: "simple-icons:typescript",
+    alt: "TypeScript logo",
+    url: "https://www.typescriptlang.org/",
+  },
+];
+
+export const BackendSkills = [
+  {
+    name: "NextJS",
+    icon: "simple-icons:nextdotjs",
+    alt: "NodeJS logo",
+    url: "https://nextjs.org/",
+  },
+  {
+    name: "AWS",
+    icon: "simple-icons:amazonaws",
+    alt: "AWS logo",
+    url: "https://aws.amazon.com/",
+  },
+  {
+    name: "Java",
+    icon: "devicon-plain:java",
+    alt: "Java logo",
+    url: "https://www.java.com/en/",
+  },
+  {
+    name: "Python",
+    icon: "simple-icons:python",
+    alt: "Python logo",
+    url: "https://www.python.org/",
+  },
+];
+
+export const OtherSkills = [
+  {
+    name: "Git",
+    icon: "simple-icons:git",
+    alt: "Git logo",
+    url: "https://git-scm.com/",
+  },
+  {
+    name: "Figma",
+    icon: "simple-icons:figma",
+    alt: "Figma logo",
+    url: "https://www.figma.com/",
+  },
+];
