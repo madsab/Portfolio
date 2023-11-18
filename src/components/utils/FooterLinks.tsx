5b7e6bb213d3b39ee46c4bc2c2731d4df1adedaa
madsab <mads.barnes@hotmail.com>
2023-11-18 13:43:02 +0100

    reduced to 4

--- src/components/utils/FooterLinks.tsx
+++ src/components/utils/FooterLinks.tsx
@@ -8,19 +8,11 @@ export const FooterLinks = [
     url: "https://www.linkedin.com/in/mads-b%C3%A5rnes-a66b96272/",
   },
   {
-    name: "Twitter",
-    url: "https://twitter.com",
-  },
-  {
     name: "Facebook",
     url: "https://www.facebook.com/profile.php?id=100007659018139",
   },
   {
     name: "Online",
-    url: "https://www.online.ntnu.no/",
-  },
-  {
-    name: "Instagram",
-    url: "https://www.instagram.com/",
+    url: "https://online.ntnu.no/",
   },
 ];
