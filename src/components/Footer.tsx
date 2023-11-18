60ecf6c2d1864c61d50f2f0bce87394e05729745
madsab <mads.barnes@hotmail.com>
2023-11-18 13:43:28 +0100

    responsive footer + email section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,22 @@
+import { on } from "events";
 import FooterLink from "./atoms/FooterLink";
+import Link from "./atoms/Link";
 import { FooterLinks } from "./utils/FooterLinks";
+import cn from "classnames";
 
 const Footer = () => {
   const FooterLinksLeft = FooterLinks.slice(0, FooterLinks.length / 2);
   const FooterLinksRight = FooterLinks.slice(FooterLinks.length / 2);
 
+  const privateEmail = "mads.barnes@hotmail.com";
+  const onlineEmail = "mads.barnes@online.ntnu.no";
+
   return (
-    <footer className="w-full bg-[#131313] grid grid-cols-4 min-h-[325px] max-sm:flex max-sm:flex-col ">
-      <div className="col-start-2 col-span-2 mx-3 my-11">
+    <footer
+      id="contact"
+      className="w-full bg-[#131313] grid grid-cols-4 min-h-[325px] max-md:flex max-md:flex-col "
+    >
+      <div className="col-start-2 col-span-2 mx-3 my-11 max-md:px-5">
         <p className="text-3xl tracking-[1rem] mb-4">Connect</p>
         <div className="flex max-sm:flex-col">
           <div className="flex-1 space-y-4">
@@ -32,7 +41,26 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="">Hei</div>
+      <div className="flex flex-col justify-center space-y-5 max-md:px-10 mb-4">
+        <div>
+          <p className="font-bold">Private Email</p>
+          <a
+            href={cn("mailto:", privateEmail)}
+            className="hover:underline opacity-50"
+          >
+            {privateEmail}
+          </a>
+        </div>
+        <div>
+          <p className="font-bold">Online Association Email</p>
+          <a
+            href={cn("mailto:", onlineEmail)}
+            className="hover:underline opacity-50"
+          >
+            {onlineEmail}
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
