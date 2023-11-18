476a3f972e97f8da61847fe66fbfe318fd82e726
madsab <mads.barnes@hotmail.com>
2023-11-19 00:00:18 +0100

    designed ProjectCard

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { FC } from "react";
 import Reveal from "../animation/Reveal";
+import { title } from "process";
+import { Icon } from "@iconify/react";
+import { JsxElement } from "typescript";
+import { Link } from "react-router-dom";
 
-const ProjectCard = ({ title, description, imgUrl, siteUrl }: any) => {
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  languages: string;
+  siteUrl: string;
+  githubUrl?: string;
+  image?: string;
+  imageAlt?: string;
+  className?: string;
+}
+const ProjectCard: FC<ProjectCardProps> = ({ ...props }) => {
+  const dot = <Icon icon="mdi:circle-medium" />;
   return (
-    <div className="w-[30.8%]">
-      <Reveal>
-        <div
-          className="group relative border-2 rounded-2xl shadow-lg shadow-black hover:cursor-pointer overflow-hidden m-4"
-          onClick={() => window.open(siteUrl, "_blank")}
-        >
-          <img
-            className="h-full object-cover group-hover:scale-125 transition-transform"
-            src={imgUrl}
-            alt="Stone School project website"
-          />
-          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70 "></div>
-          <div className="absolute inset-0 flex flex-col items-center px-9 text-center translate-y-[75%] group-hover:translate-y-0 group-hover:justify-center transition-all">
-            <p className="text-3xl font-bold mb-3">{title}</p>
-            <p className="text-lg font-light italic text-slate-300">
-              {description}
-            </p>
-          </div>
-        </div>
-      </Reveal>
+    <div className="w-full relative">
+      <Link to={props.siteUrl} target="_blank">
+        <img
+          src={props.image}
+          alt={props.imageAlt}
+          className="rounded-3xl w-full h-4/5 object-cover cursor-pointer hover:scale-105 hover:mb-3 transition-all duration-200 ease-linear "
+        />
+      </Link>
+      <p className="absolute text-7xl bottom-1/2 -left-20 font-extrabold h-0">
+        {props.title}
+      </p>
+      <div className="flex items-center justify-center font-light">
+        {props.title}
+        {dot}
+        {props.description}
+        {dot}
+        {props.languages}
+      </div>
     </div>
   );
 };
