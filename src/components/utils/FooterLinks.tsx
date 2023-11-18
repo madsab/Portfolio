32281b4a9731f2ad87bd2d09599f2487f8def04d
madsab <mads.barnes@hotmail.com>
2023-11-18 12:06:25 +0100

    add footerlinks used in footer

---
+++ src/components/utils/FooterLinks.tsx
@@ -0,0 +1,26 @@
+export const FooterLinks = [
+  {
+    name: "Github",
+    url: "https://github.com",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://linkedin.com",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com",
+  },
+  {
+    name: "Facebook",
+    url: "https://facebook.com",
+  },
+  {
+    name: "Online",
+    url: "https://www.online.ntnu.no/",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/",
+  },
+];
