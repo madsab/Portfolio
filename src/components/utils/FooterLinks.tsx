cf8adefcd5f44204b36f825e863f917382534c63
madsab <mads.barnes@hotmail.com>
2023-11-18 12:06:35 +0100

    update links

--- src/components/utils/FooterLinks.tsx
+++ src/components/utils/FooterLinks.tsx
@@ -1,11 +1,11 @@
 export const FooterLinks = [
   {
     name: "Github",
-    url: "https://github.com",
+    url: "https://github.com/madsab",
   },
   {
     name: "LinkedIn",
-    url: "https://linkedin.com",
+    url: "https://www.linkedin.com/in/mads-b%C3%A5rnes-a66b96272/",
   },
   {
     name: "Twitter",
@@ -13,7 +13,7 @@ export const FooterLinks = [
   },
   {
     name: "Facebook",
-    url: "https://facebook.com",
+    url: "https://www.facebook.com/profile.php?id=100007659018139",
   },
   {
     name: "Online",
