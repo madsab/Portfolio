3d32cd93e32ef3217e4283e8002343769f0c6886
madsab <mads.barnes@hotmail.com>
2023-11-19 21:05:08 +0100

    style projectCard

--- src/components/atoms/ProjectCard.tsx
+++ src/components/atoms/ProjectCard.tsx
@@ -5,7 +5,7 @@ import { Icon } from "@iconify/react";
 import { JsxElement } from "typescript";
 import { Link } from "react-router-dom";
 
-interface ProjectCardProps {
+export interface ProjectCardProps {
   title: string;
   description: string;
   languages: string;
@@ -18,15 +18,15 @@ interface ProjectCardProps {
 const ProjectCard: FC<ProjectCardProps> = ({ ...props }) => {
   const dot = <Icon icon="mdi:circle-medium" />;
   return (
-    <div className="w-full relative">
+    <div className=" max-w-[700px] relative ">
       <Link to={props.siteUrl} target="_blank">
         <img
           src={props.image}
           alt={props.imageAlt}
-          className="rounded-3xl w-full h-4/5 object-cover cursor-pointer hover:scale-105 hover:mb-3 transition-all duration-200 ease-linear "
+          className="rounded-3xl w-full object-cover cursor-pointer hover:scale-105 hover:mb-3 transition-all duration-200 ease-linear "
         />
       </Link>
-      <p className="absolute text-7xl bottom-1/2 -left-20 font-extrabold h-0">
+      <p className="absolute text-7xl bottom-[40%] -left-20 font-extrabold h-0">
         {props.title}
       </p>
       <div className="flex items-center justify-center font-light">
