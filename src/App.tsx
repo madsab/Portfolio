41b784d438fcf8354662de3dff6bddfd33526b57
madsab <mads.barnes@hotmail.com>
2023-11-19 21:03:53 +0100

    new design

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,22 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import About from "./components/About";
 import SoMeContainer from "./components/organisms/SoMeContainer";
+import Footer from "./components/Footer";
 
 function App() {
   const [trigger, setTrigger] = useState(true);
   return (
     <div
       id="top"
-      className=" relative overflow-x-clip bg-gradient-to-r from-theme-gray to-theme-brown text-theme-text w-full "
+      className=" relative overflow-x-clip bg-gradient-to-b from-[#202020] text-theme-text w-full"
     >
       <Navbar />
-      <SoMeContainer className="sticky top-[65%] h-1 ml-7 max-sm:hidden" />
+      <SoMeContainer className="sticky top-[65%] h-1 ml-7 z-10 max-md:hidden" />
       <Banner />
       <About />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
