9059ff55155f9c91caf15911db91af7f9f879e0a
madsab <mads.barnes@hotmail.com>
2023-11-19 21:05:22 +0100

    add Carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,33 +1,21 @@
-import React from "react";
 import ProjectCard from "./atoms/ProjectCard";
 import Reveal from "./animation/Reveal";
+import { ProjectLinks } from "./utils/ProjectLinks";
+import Carousel from "./organisms/Carousel";
 
 const Projects = () => {
-  const projects = [
-    {
-      title: "STONE - SpyWebsite",
-      description:
-        "School Project: Frontend design and development - Draw Whey icon to Dag's hand",
-
-      siteUrl: "https://folk.ntnu.no/madsab/STONE/website/index.html",
-    },
-    {
-      title: "Porfolio - This site",
-      description: "Personal portfolio to display me, my skills and projects",
-
-      siteUrl: "#",
-    },
-  ];
-
   return (
-    <div className=" w-full flex flex-col items-center" id="projects">
+    <div
+      id="projects"
+      className=" w-full flex flex-col items-center justify-evenly min-h-screen"
+    >
       <Reveal>
         <p className="text-5xl mb-3 font-bold">My Projects</p>
       </Reveal>
-      <div className="border-2 rounded-xl w-[90%] flex justify-center flex-wrap shadow-2xl">
-        {projects.map((p, index) => {
-          return <ProjectCard key={index} {...p} />;
-        })}
+      <div className="flex w-full justify-center relative ">
+        <div className=" w-full rounded-xl flex justify-center shadow-2xl">
+          <Carousel children={ProjectLinks} />
+        </div>
       </div>
     </div>
   );
