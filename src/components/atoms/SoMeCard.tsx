779bb447cf69ecd19533afa48f2705489a0b5b29
madsab <mads.barnes@hotmail.com>
2023-11-19 21:04:56 +0100

    add animation

--- src/components/atoms/SoMeCard.tsx
+++ src/components/atoms/SoMeCard.tsx
@@ -12,7 +12,7 @@ const SoMeCard: React.FC<SoMeCardProps> = ({ url, logo }) => {
     <Link
       to={url}
       target="_blank"
-      className="text-2xl rounded-full p-2 cursor-pointer  hover:bg-theme-secondary hover:text-theme-accent"
+      className="text-2xl rounded-full p-2 cursor-pointer  hover:bg-theme-secondary hover:text-theme-accent transition-all duration-200"
     >
       <Icon icon={logo} width="30" height="30" className="" />
     </Link>
