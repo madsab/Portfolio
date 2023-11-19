5118c8c50c2b50d4d757983bcc9ad2066e813b3b
madsab <mads.barnes@hotmail.com>
2023-11-19 21:04:33 +0100

    add projects

---
+++ src/components/utils/ProjectLinks.tsx
@@ -0,0 +1,32 @@
+import StoneImg from "../../assets/images/stone.png";
+import Dredge from "../../assets/images/Dredge_06.jpeg";
+
+export const ProjectLinks = [
+  {
+    title: "STONE",
+    description: "Frontend & Design",
+    languages: "HTML, CSS, JavaScript",
+    siteUrl: "https://folk.ntnu.no/madsab/STONE/website/",
+    githubUrl: "https://github.com/madsab/stone",
+    image: Dredge,
+    imageAlt: "Stone Website",
+  },
+  {
+    title: "STONE",
+    description: "Frontend & Design",
+    languages: "HTML, CSS, JavaScript",
+    siteUrl: "https://folk.ntnu.no/madsab/STONE/website/",
+    githubUrl: "https://github.com/madsab/stone",
+    image: Dredge,
+    imageAlt: "Stone Website",
+  },
+  {
+    title: "STONE",
+    description: "Frontend & Design",
+    languages: "HTML, CSS, JavaScript",
+    siteUrl: "https://folk.ntnu.no/madsab/STONE/website/",
+    githubUrl: "https://github.com/madsab/stone",
+    image: Dredge,
+    imageAlt: "Stone Website",
+  },
+];
