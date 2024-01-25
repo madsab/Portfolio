1c9333a5958a44195babc8100295089b36414e84
madsab <mads.barnes@hotmail.com>
2024-01-25 17:54:40 +0100

    update projects to realistic

--- src/components/utils/ProjectLinks.tsx
+++ src/components/utils/ProjectLinks.tsx
@@ -1,32 +1,28 @@
 import StoneImg from "../../assets/images/stone.png";
 import Dredge from "../../assets/images/Dredge_06.jpeg";
+import Portfolio from "../../assets/images/portfolio.png";
+import { ProjectCardProps } from "../atoms/ProjectCard";
 
-export const ProjectLinks = [
+export const ProjectLinks: ProjectCardProps[] = [
   {
     title: "STONE",
     description: "Frontend & Design",
     languages: "HTML, CSS, JavaScript",
+    extra: "Move the Whey icon into Dag's hand to reveal the website",
     siteUrl: "https://folk.ntnu.no/madsab/STONE/website/",
     githubUrl: "https://github.com/madsab/stone",
-    image: Dredge,
+    image: StoneImg,
     imageAlt: "Stone Website",
+    border: true,
   },
   {
-    title: "STONE",
+    title: "Portfolio",
     description: "Frontend & Design",
-    languages: "HTML, CSS, JavaScript",
-    siteUrl: "https://folk.ntnu.no/madsab/STONE/website/",
-    githubUrl: "https://github.com/madsab/stone",
-    image: Dredge,
-    imageAlt: "Stone Website",
-  },
-  {
-    title: "STONE",
-    description: "Frontend & Design",
-    languages: "HTML, CSS, JavaScript",
-    siteUrl: "https://folk.ntnu.no/madsab/STONE/website/",
-    githubUrl: "https://github.com/madsab/stone",
-    image: Dredge,
-    imageAlt: "Stone Website",
+    languages: "React, Typescript, TailwindCSS",
+    siteUrl: "https://madsbarnes.com",
+    githubUrl: "https://github.com/madsab/portfolio",
+    image: Portfolio,
+    imageAlt: "Portfolio Website",
+    border: true,
   },
 ];
