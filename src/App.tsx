f50bcd35fcf05d24a386caf7630df7fc2774eb6a
madsab <mads.barnes@hotmail.com>
2024-01-27 17:59:07 +0100

    add new routes + move App to indexpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
-import Banner from "./components/Banner";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import About from "./components/About";
-import SoMeContainer from "./components/organisms/SoMeContainer";
-import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IndexPage from "./pages/indexPage";
 
 function App() {
-  const [trigger, setTrigger] = useState(true);
   return (
-    <div
-      id="top"
-      className="bg-gradient-to-b from-[#202020] text-theme-text w-screen h-screen overflow-auto snap-mandatory snap-y"
-    >
-      <Navbar />
-      <SoMeContainer className="sticky top-[65%] h-1 ml-7 z-10 max-md:hidden" />
-      <Banner />
-      <About />
-      <Skills />
-      <Projects />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/indexPage.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import About from "../components/About";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import SoMeContainer from "../components/organisms/SoMeContainer";
+
+const IndexPage = () => {
+  return (
+    <div
+      id="top"
+      className="bg-gradient-to-b from-[#202020] text-theme-text w-screen h-screen overflow-auto snap-mandatory snap-y"
+    >
+      <Navbar />
+      <SoMeContainer className="sticky top-[65%] h-1 ml-7 z-10 max-md:hidden" />
+      <Banner />
+      <About />
+      <Skills />
+      <Projects />
+      <Footer />
+    </div>
+  );
+};
+
+export default IndexPage;
