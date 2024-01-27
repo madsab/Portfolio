07a424529eed77fd0f0d193419c58ecf6740b3c1
madsab <mads.barnes@hotmail.com>
2024-01-27 17:52:39 +0100

    eslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CV from "./pages/CV";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
