8f8cb5f8b9ca614395ac8e5e57be35053acd4625
madsab <mads.barnes@hotmail.com>
2024-04-19 22:22:28 +0200

    add: responsive project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,12 +9,12 @@ const Projects = () => {
       id="projects"
       className=" w-screen flex flex-col items-center snap-start"
     >
-      <div className=" snap-mandatory snap-y">
+      <div className=" snap-mandatory snap-y max-md:space-y-48 ">
         {ProjectLinks.map((project, index) => {
           return (
             <div
               key={index}
-              className=" h-screen snap-start flex flex-col justify-center items-center"
+              className="md:h-screen snap-start flex flex-col justify-center items-center"
             >
               {index == 0 && (
                 <Reveal className="p-8">
